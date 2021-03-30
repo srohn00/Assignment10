cd8c12e4c44bbf9b9d96e886ae0ade3e3e2827a5
ShelbyR <70963984+srohn00@users.noreply.github.com>
2021-03-30 10:05:23 -0600

    Add project files.

---
+++ Components/BowlerTeamViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Assignment10.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Components
+{
+    public class BowlerTeamViewComponent : ViewComponent
+    {
+        private BowlingLeagueContext context;
+        public BowlerTeamViewComponent (BowlingLeagueContext ctx)
+        {
+            context = ctx;
+        }
+        public IViewComponentResult Invoke()
+        {
+                      return View(context.Teams
+                          //.Select(x => x.TeamName)
+                          .Distinct()
+                          .OrderBy(x => x)
+                          //.ToList()
+                          );
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Assignment10.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BowlingLeagueContext context { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, BowlingLeagueContext ctx)
+        {
+            _logger = logger;
+            context = ctx;
+        }
+
+        public IActionResult Index(long? bowlerteamid)
+        {
+            //var blah = "%oooword%"; //for string interpolation>>replace bowlerSearch w/ blah below
+            //or could pass in a parameter to change search var each time
+                //index(string bowlerSearch) example
+
+            return View(context.Bowlers
+                //.FromSqlRaw("SELECT * FROM Bowlers WHERE BowlerFirstName LIKE \"%A%\" ORDER BY BowlerFirstName")
+                //.FromSqlInterpolated($"SELECT * FROM Bowlers WHERE TeamId LIKE {teamSearch}")
+                .FromSqlInterpolated($"SELECT * FROM Bowlers WHERE TeamId = {bowlerteamid} OR {bowlerteamid} IS NULL")
+                //.Where(x => x.BowlerFirstName.Contains("A"))
+                //.OrderBy(x => x.BowlerId)
+                .ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
